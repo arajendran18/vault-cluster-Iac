8c7c9761972fb1180f5d193c12b6084a839462c2
Ajithkumar R <168090251+arajendran18@users.noreply.github.com>
2025-07-06 11:34:19 +0530

    Update security.tf

--- security.tf
+++ security.tf
@@ -7,14 +7,14 @@ resource "aws_security_group" "vault_sg" {
     from_port   = 8200
     to_port     = 8200
     protocol    = "tcp"
-    cidr_blocks = var.allowed_ip_cidr_blocks
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = var.allowed_ip_cidr_blocks
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
