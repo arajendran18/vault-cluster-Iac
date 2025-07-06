025b94fa463870c61624c776096909b1f797e3b9
arajendran18 <ajithkumar.adr18@gmail.com>
2025-07-06 09:21:32 +0530

    vpc module removed

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
 provider "aws" {
   region = var.region
 }
-
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-  version = "3.18.1"
-  # Optionally add VPC setup if needed, else pass existing VPC
-}
