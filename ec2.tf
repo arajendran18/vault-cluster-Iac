6cff9d4513eee18f6f8b149b0a47dbbef1c98742
Ajithkumar R <168090251+arajendran18@users.noreply.github.com>
2025-07-06 10:56:41 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,18 @@
 resource "aws_instance" "vault" {
-  count         = var.vault_instance_count
+  count         = 2
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_name
-  subnet_id     = element(var.public_subnet_ids, count.index)
-  security_groups = [aws_security_group.vault_sg.id]
-  iam_instance_profile = aws_iam_instance_profile.vault_profile.name
-  user_data = file("user_data.sh")
+
+  subnet_id = element(
+    ["subnet-0f6709602b301aa95", "subnet-0e2be7faaf7472165"],
+    count.index
+  )
+
+  security_groups       = [aws_security_group.vault_sg.id]
+  iam_instance_profile  = aws_iam_instance_profile.vault_profile.name
+  user_data             = file("user_data.sh")
+
   tags = {
     Name = "vault-${count.index + 1}"
   }
