1389a2e49c515b9b66f08bb539e257b684a53ab4
Ajithkumar R <168090251+arajendran18@users.noreply.github.com>
2025-07-06 10:58:48 +0530

    Update nlb.tf

--- nlb.tf
+++ nlb.tf
@@ -2,7 +2,10 @@ resource "aws_lb" "vault_nlb" {
   name               = "vault-nlb"
   internal           = false
   load_balancer_type = "network"
-  subnets            = var.public_subnet_ids
+  subnets = [
+    "subnet-0f6709602b301aa95",
+    "subnet-0e2be7faaf7472165"
+  ]
 }
 
 resource "aws_lb_target_group" "vault_tg" {
@@ -24,7 +27,7 @@ resource "aws_lb_listener" "vault_listener" {
 }
 
 resource "aws_lb_target_group_attachment" "vault_tg_attachment" {
-  count            = var.vault_instance_count
+  count            = aws_instance.vault.*.id != [] ? length(aws_instance.vault) : 0
   target_group_arn = aws_lb_target_group.vault_tg.arn
   target_id        = aws_instance.vault[count.index].id
   port             = 8200
